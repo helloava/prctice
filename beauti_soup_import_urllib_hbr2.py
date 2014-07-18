74f12e42e7b2f745d13bcbb3b899836ce7fe5010
ah young koo <helloavakoo@gmail.com>
2014-07-18 11:40:24 +0900

    biz title

---
+++ beauti_soup_import_urllib_hbr2.py
@@ -0,0 +1,14 @@
+import urllib
+
+from bs4 import BeautifulSoup
+
+htmltext = urllib.urlopen("http://hbr.org/").read()
+
+soup = BeautifulSoup(htmltext)
+
+authors = []
+
+for tag in soup.select(".item-title"):
+	authors.append(tag.get_text())
+for author in authors:
+	print author

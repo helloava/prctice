55144c867431b7483c28cc6f1628cece8374d652
ah young koo <helloavakoo@gmail.com>
2014-07-21 15:01:44 +0900

    commit

--- beauti_soup_import_urllib_ibr.py
+++ beauti_soup_import_urllib_ibr.py
@@ -2,13 +2,14 @@ import urllib
 
 from bs4 import BeautifulSoup
 
-htmltext = urllib.urlopen("http://hbr.org/").read()
+htmltext = urllib.urlopen("https://www.edx.org/course-list").read()
 
 soup = BeautifulSoup(htmltext)
 
 authors = []
 
-for tag in soup.select(".item-title"):
+for tag in soup.select(".new-course-ribbon"):
 	authors.append(tag.get_text())
 for author in authors:
 	print author
+

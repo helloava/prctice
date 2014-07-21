f3a6b948f40425a6c95a8061abaa7754e8fcc289
ah young koo <helloavakoo@gmail.com>
2014-07-21 14:57:05 +0900

    add decode

--- import_urllib_ibr.py
+++ import_urllib_ibr.py
@@ -7,8 +7,7 @@ def ent_title(pageNum):
 	html = ent.read().split('<article class="item-list">')
 
 	for data in html[1:]: 
-	 	print data.split('</a>')[0].split('">')[2]
- 
+	 	print data.split('</a>')[0].split('">')[2].decode("utf-8") 
 		#print toon
 for i in range(1,3) :
 	ent_title(i)
